5f2cadc6028b8d81a512c6f3954fa7f993374b02
Маков Андрей Вячеславович <av_makov@bingo-boom.com>
2019-08-22 12:50:05 +0300

    obtain inside hook

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 import {stream} from "m2"
-import adaptive from "./adaptive"
-import kit from "./kit"
-import keyframes from "./keyframes"
-import customPathname from "./custom-pathname"
 
 export default {
-
-    keyframes,
-    owner() {
-        return stream( (emt, { hook } ) => {
-
-            emt([{}]);
-
-            hook.add( ( { key, event } ) => {
-
-                event.log();
-
-            } );
-
-        } );
-
-    },
-    lazy() {
-        return stream( (emt) => {
-            setTimeout( () => {
-                emt([{lazydata: 777}]);
-            }, 5000 );
-        } );
-    },
-    adaptive,
-    kit,
-    ["custom-pathname"]: customPathname
-
+    model1: ({obtain}) =>
+        stream((emt, {hook, sweep}) => {
+            emt([]);
+            const model3Hook = obtain("@model3").at(()=>{});
+            sweep.add(model3Hook);
+            hook.add(() => {
+                const model2Hook = obtain("@model2").at(()=>{});
+                sweep.add(model2Hook);
+                model2Hook({});
+                model3Hook({});
+                emt([]);
+            });
+        }),
+    model2: () =>
+        stream((emt, {hook}) => {
+            emt([]);
+            hook.add(({action}) => {
+                document.querySelector(".content").innerHTML += "model2 handler ";
+                emt([]);
+            });
+        }),
+    model3: () =>
+        stream((emt, {hook}) => {
+            emt([]);
+            hook.add(() => {
+                console.log("here");
+                document.querySelector(".content").innerHTML += "model3 handler ";
+                emt([]);
+            });
+        }),
 }
